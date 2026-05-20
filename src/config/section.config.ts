2db57665b8420ce0cb37d421392e03e390416b06
Muhammad Huzaifa Yaqoob <muhammadhuzaifayaqoob3@gmail.com>
2026-05-20 12:05:28 +0500

    fixes ts issue

--- src/config/section.config.ts
+++ src/config/section.config.ts
@@ -8,6 +8,16 @@ export const sectionSchema = z
     main_image: z.string().optional(), // Changed to optional as it was removed from service.md
     divider_text: z.string().optional(), // Added as optional
     badge: z.string().optional(), // Added as optional
+    tag: z.string().optional(),
+    core_strengths: z.array(z.string()).optional(),
+    technologies: z
+      .array(
+        z.object({
+          name: z.string(),
+          icon: z.string(),
+        })
+      )
+      .optional(),
     // Add any other common properties that all sections might share
   })
   .passthrough() // Allows for additional, specific properties in individual section front matters
